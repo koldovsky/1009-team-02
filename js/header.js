afc3355c2d77f1dff7de0941c6287c1c62520746
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-05-16 12:41:12 +0000

    Added navigation on the page through id

--- js/header.js
+++ js/header.js
@@ -3,4 +3,17 @@ const offScreenMenu = document.querySelector('.header__off-screen-menu');
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
-})
+  document.body.classList.toggle("lock");
+});
+
+const navLinks = document.querySelectorAll(".header__menu-link");
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", (_) => {
+    if (hamburgerMenu.classList.contains("active")) {
+      document.body.classList.remove("lock");
+      hamburgerMenu.classList.remove("active");
+      offScreenMenu.classList.remove("active");
+    }
+  }
+  );
+});
