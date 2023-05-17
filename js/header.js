2f3e5e3f79e86fb91905e4eacaad22a200d5206a
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-05-17 16:04:43 +0000

    Updated header

--- js/header.js
+++ js/header.js
@@ -3,18 +3,18 @@ const offScreenMenu = document.querySelector('.header__off-screen-menu');
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
-  document.body.classList.toggle("lock");
+  document.body.classList.toggle('lock');
   document.body.classList.toggle('lock-scroll');
 });
 
-const navLinks = document.querySelectorAll("#navigation-link");
+const navLinks = document.querySelectorAll('#navigation-link');
 navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", (_) => {
-    if (hamburgerMenu.classList.contains("active")) {
-      document.body.classList.remove("lock");
+  navLink.addEventListener('click', (_) => {
+    if (hamburgerMenu.classList.contains('active')) {
+      document.body.classList.remove('lock');
       document.body.classList.remove('lock-scroll');
-      hamburgerMenu.classList.remove("active");
-      offScreenMenu.classList.remove("active");
+      hamburgerMenu.classList.remove('active');
+      offScreenMenu.classList.remove('active');
     }
   }
   );
