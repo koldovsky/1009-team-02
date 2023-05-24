5a4513caa4af4aae5e5b5fa5922ef3317f88779a
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-05-24 17:00:14 +0300

    Updated solution for task_3

--- js-tasks/task_3.js
+++ js-tasks/task_3.js
@@ -36,3 +36,31 @@ function buildFun(n) {
   }
   return res
 }
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status, master);
+    this.master = master;
+  }
+  introduce() {
+    return super.introduce();
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
